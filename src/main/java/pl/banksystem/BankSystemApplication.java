133afd158ab498d85594443c5846bdb8417f1f68
Michał Jemiołek <81627280+mjemiolek@users.noreply.github.com>
2022-06-01 23:41:38 +0200

    EmailSenderService

--- src/main/java/pl/banksystem/BankSystemApplication.java
+++ src/main/java/pl/banksystem/BankSystemApplication.java
@@ -1,22 +1,38 @@
 package pl.banksystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.banksystem.logic.domain.AppUser;
 import pl.banksystem.logic.domain.Role;
+import pl.banksystem.logic.service.EmailSenderService;
 import pl.banksystem.logic.service.UserService;
+
+import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class BankSystemApplication {
+    //@Autowired
+    //private EmailSenderService senderService;
 
     public static void main(String[] args) {
         SpringApplication.run(BankSystemApplication.class, args);
     }
 
+/*    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail() throws MessagingException {
+        senderService.sendMail("michal.jemiolek@gmail.com","Email from java bank system",
+                "Hello user.\nWe are sorry but you don't have creditworthiness to take a loan in our bank");
+        senderService.sendMessageWithAttachment("michal.jemiolek@gmail.com","Email from java bank system",
+                "Hello user.","C:\\Users\\plik.gif");
+    }
+*/
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
